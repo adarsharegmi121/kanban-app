c16f091ff7660c88ec08918852116a697cee1c86
adarsharegmi121 <adarsha.regmi@cotiviti.com>
2022-08-25 20:40:54 +0545

    refactor code

--- src/backend_src/kanban_task/serializers.py
+++ src/backend_src/kanban_task/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from kanban_task.models import KanbanTask
 
-class KanbaTaskSerializer(serializers.ModelSerializer):
+class KanbanTaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = KanbanTask
         fields = ["title", "description", "status", "lane"]
